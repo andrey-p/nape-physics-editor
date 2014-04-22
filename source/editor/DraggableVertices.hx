41fe0f9f40d1915a66c834e7819a5f4dd0700c46
Andrey Pissantchev <pissantchev@gmail.com>
2014-04-22 16:42:22 +0100

    Fixed the logic that checks whether a polygon is valid
    Figured the best way will be to use Nape's own way of figuring this out
    using the GeomPoly class - seeing how we're catering to Nape's own
    definition of a valid polygon.

--- source/editor/DraggableVertices.hx
+++ source/editor/DraggableVertices.hx
@@ -4,6 +4,7 @@ import flixel.group.FlxTypedGroup;
 import flixel.FlxSprite;
 import flixel.FlxG;
 
+import nape.geom.GeomPoly;
 import nape.shape.Shape;
 import nape.shape.Polygon;
 import nape.geom.Vec2;
@@ -48,47 +49,20 @@ class DraggableVertices extends FlxTypedGroup<DraggableVertex> {
         });
     }
 
-    private function isPolygonConvex():Bool {
-            // check if it's a convex polygon, taken from SO:
-            // http://stackoverflow.com/a/1881201/484538
+    private function isPolygonValid():Bool {
+        // piggyback on nape's own polygon validation logic
+        var vectors = new Array<Vec2>();
 
-            var v1:DraggableVertex;
-            var v2:DraggableVertex;
-            var v3:DraggableVertex;
-            var dx1:Float;
-            var dy1:Float;
-            var dx2:Float;
-            var dy2:Float;
-            var zComponents:Array<Float> = new Array<Float>();
-            for (i in 0...members.length) {
-                v1 = members[i];
-                v2 = members[i + 1];
-                if (v2 == null) {
-                    v2 = members[members.length - i];
-                }
-                v3 = members[i + 2];
-                if (v3 == null) {
-                    v3 = members[members.length - i + 1];
-                }
+        for (v in members) {
+            vectors.push(Vec2.weak(v.x, v.y));
+        }
 
-                dx1 = v2.x - v1.x;
-                dy1 = v2.y - v1.y;
-                dx2 = v3.x - v2.x;
-                dy2 = v3.y - v2.y;
-                zComponents.push(dx1 * dy2 - dy1 * dx2);
-            }
+        var poly = GeomPoly.get(vectors);
+        var isValid:Bool = poly.isSimple() && poly.isConvex();
 
-            // check if all the components are either negative or positive
-            // ... there must be a more elegant way of doing this
-            var positiveZComponents:Array<Float> = zComponents.filter(function (i):Bool {
-                return i >= 0;
-            });
+        poly.dispose();
 
-            trace(positiveZComponents.length);
-            trace(zComponents.length);
-
-            return positiveZComponents.length == zComponents.length
-                || positiveZComponents.length == 0;
+        return isValid;
     }
 
     public override function update():Void {
@@ -102,7 +76,7 @@ class DraggableVertices extends FlxTypedGroup<DraggableVertex> {
                 }
             }
         } else if (dragging && FlxG.mouse.justReleased) {
-            if (isPolygonConvex()) {
+            if (isPolygonValid()) {
                 currentVertex.commitDrag();
             } else {
                 currentVertex.rejectDrag();
